689d5a7dc894462503798b08735c5c51a329156b
Lee-yeonjin <lyjduswls37@naver.com>
2025-12-16 15:22:42 +0900

    workflow test commit

---
+++ Desktop/employment/backend-pass-portfolio-main/src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
@@ -0,0 +1,17 @@
+package ding.co.backendportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableJpaAuditing
+@SpringBootApplication
+@EnableScheduling
+public class BackendportfolioApplication {
+    public static void main(String[] args) {
+        System.out.println("연진 .. 화이팅");
+        SpringApplication.run(BackendportfolioApplication.class, args);
+    }
+
+}
